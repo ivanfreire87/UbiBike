ab4d4ff1e32d1c515c12640f0e486126bd5d96c4
ivan <ivanfreire87@gmail.com>
2016-05-08 18:10:29 +0100

    3 actividades

--- app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/MainActivity.java
@@ -1,46 +1,36 @@
 package pt.ulisboa.tecnico.cmov.ubibike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "pt.ulisboa.tecnico.cmov.ubibike.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "UBLog MainActivity created.");
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, SendMessageActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    public void sendPoints(View view) {
+        Intent intent = new Intent(this, SendPointsActivity.class);
+        startActivity(intent);
     }
 
 
---
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/ubibike/SendMessageActivity.java
@@ -0,0 +1,21 @@
+package pt.ulisboa.tecnico.cmov.ubibike;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+/**
+ * Created by ivanf on 08/05/2016.
+ */
+public class SendMessageActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_send_message);
+    }
+
+}
